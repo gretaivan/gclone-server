b9b1277e9588724d2062bb7d23ca5abec6d9ae44
gretaivan <greta.ivan91@gmail.com>
2021-03-09 16:08:35 +0000

    adds TODO in the server.js

--- server.js
+++ server.js
@@ -7,13 +7,55 @@ server.use(bodyParser.json());
 server.get('/', (req, res) => {
     res.status(200).send('Welcome to gclone api')
 })
+//TODO: this the route ('/') say hello
 
-// server.get('/poll?:query', (req, res) => {
-//     res.status(200).send('top 10 responses as json')
-// })
+
+// TODO: this route (search?:keyword)  req.params.keyword receive req from client
+
+    //TODO:  request from zenzer API 
+
+
+//TODO: return the query result
+
+
+//TODO: format results 
+
+
+//TODO: extract
+
+
+//TODO: Feeling lucky 
 
 // server.get('/poll/lucky?:query', (req, res) => {
 //     res.status(200).send('link to random response')
 // })
+
+
+    //reuse req from client
+
+
+    //reuse request from zenzer
+
+
+    //reuse generate random index 0 - 9 
+
+
+    // redirect user in randomIndex result
+
+
+
+//TODO generate random index 0 - 9 
+
+
+
+//TODO get from zenzer by keyword  
+//server.get('/poll?:query', (req, res) => {
+//     res.status(200).send('top 10 responses as json')
+// })
+
+
+
+
+
 
 module.exports = server;
